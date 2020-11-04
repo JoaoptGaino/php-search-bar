76b28b129c0b316cc21de7e1a37708db32e0afd8
joaoptgaino <joaoptgaino@hotmail.com>
2020-11-04 09:22:52 -0300

    First commit

---
+++ search.js
@@ -0,0 +1,48 @@
+var nome;
+
+function handleChange() {
+    var x = document.getElementById("inp");
+    nome = document.getElementById("nome");
+    if (x.value == '') {
+        getAll();
+        return;
+    }
+    procura(x.value);
+}
+
+function getAll() {
+    startAjax();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            if (this.responseText != '') {
+                document.getElementById("nome").innerHTML = this.responseText;
+            }
+        }
+    }
+    xmlhttp.open("GET", "getAllDAO.php");
+    xmlhttp.send();
+}
+
+function procura(proc) {
+    startAjax();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            if (this.responseText != '') {
+                console.log(this.responseText);
+                document.getElementById("nome").innerHTML = this.responseText;
+            } else {
+                console.log("Sem resposta.");
+            }
+        }
+    };
+    xmlhttp.open("GET", `searchDAO.php?q=${proc}`);
+    xmlhttp.send();
+}
+
+function startAjax() {
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP")
+    }
+}
---
+++ searchDAO.php
@@ -0,0 +1,31 @@
+<?php
+
+$con = mysqli_connect('localhost', 'root', 'mgtrda', 'pagination');
+
+$sql = "SET NAMES 'utf8'";
+
+mysqli_query($con, $sql);
+
+$sql = 'SET character_set_connection=utf8';
+
+mysqli_query($con, $sql);
+
+$sql = 'SET character_set_client=utf8';
+
+mysqli_query($con, $sql);
+
+$sql = 'SET character_set_results=utf8';
+
+mysqli_query($con, $sql);
+
+$search = $_GET['q'];
+$sql = "SELECT * FROM informacoes WHERE nome LIKE '$search%'";
+$result = mysqli_query($con, $sql);
+
+while ($row = mysqli_fetch_array($result)) {
+    $tabela = "<tr>";
+    $tabela .= "<td>" . $row['nome'] . "</td>";
+    $tabela .= "<td>" . $row['telefone'] . "</td>";
+    $tabela .= "</tr>";
+    echo $tabela;
+}
